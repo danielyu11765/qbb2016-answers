0e15d111f86f5b1a9947a6e68b59abe461983d72
danielyu11765 <danielyu11765@gmail.com>
2016-09-05 20:48:08 -0400

    recommit Q1

--- day3-homework/day3-homework01.py
+++ day3-homework/day3-homework01.py
@@ -6,6 +6,8 @@ Read sequencne from a fasta file count the number of times each
 k-mer occurs across all sequences and print kmers and counts.
 
 usage: 01-kmer-counter.py k < fasta_file
+
+This is k-mer matcher
 """
 
 
@@ -16,50 +18,37 @@ import sys, fasta
 k = int(sys.argv[1])
 
 for ident, sequence in fasta.FASTAReader(open(sys.argv[2])): # query file open
-  kmer_counts = {}
+  kmer_counts = {} #this is a dictionary
   
   sequence = sequence.upper()
   for i in range(0, len(sequence) - k):
     kmer = sequence[i : i + k]
     if kmer not in kmer_counts:
       kmer_counts[kmer] = []   #get the i and store in the dictionary
-      kmer_counts[kmer].append(i) 
+      kmer_counts[kmer].append(i + 1)
       continue
-    kmer_counts[kmer].append(i) 
+    kmer_counts[kmer].append(i + 1) #update kmer_counts if it gets the same kmer !!!!!!
+    #kmer_counts = kmer, kmer_counts[kmer]
+
+#for kmer in sorted(kmer_counts, key=kmer_counts.get, reverse=False):#search iteritems !!!!!! .get !!use the function .get to sort the key=kmer_counts in kmer
+    
+    #print kmer, kmer_counts[kmer]
+     
+for ident2, sequence2 in fasta.FASTAReader(open(sys.argv[3])): #target open  
   
-    
-for ident2, sequence2 in fasta.FASTAReader(open(sys.argv[3])): #target open
-  for i in range(0, len(sequence2) - k):
-    kmer2 = sequence[i : i + k]
-    if kmer2 in kmer_counts:
-      target_gene = ident2
-      target_position = str(i)
-      query_position = " ".join(map(str, kmer_counts[kmer2]))
-    
-    print "\t".join([target_gene, target_position, query_position, kmer2])
+  sequence2 = sequence2.upper()
+  for i2 in range(0, len(sequence2) - k):
+    kmer2 = sequence[i2 : i2 + k]    
+    if kmer2 in kmer_counts:      
+      t_gene = ident2
+      t_position = str(i2 + 1)
+      q_position = " ".join(map(str, kmer_counts[kmer2]))
+      print "\t".join([t_gene, t_position, q_position, kmer2])
       
       
-    
-    
+      
+      
 
-#for kmer in kmer_counts:
-#    print kmer, kmer_counts[kmer]
-
-#for kmer, count in kmer_counts.iteritems(): #search iteritems !!!!!!
-    #print kmer, count
-
-
-
-
-
-#for kmer in kmer_counts:
-  #print kmer, kmer_counts[kmer]
-    
-    #if kmer not in kmer_counts:   #this is as same a previous one
-      #kmer_counts[kmer] = 1
-    #else:
-      #kmer_counts[kmer] += 1
-    
     
     
     
