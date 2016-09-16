0b257952fc282bea60f1f5588a6e7d4a01c67518
danielyu11765 <danielyu11765@gmail.com>
2016-09-16 13:19:54 -0400

    week1 homework

--- week1-homework/AAtoNT-02.py
+++ week1-homework/AAtoNT-02.py
@@ -1,64 +1,41 @@
 #!/usr/bin/env python
 
-""""
-
-Parse every FASTA record from stdin and print it
-
-"""
-
 import sys
+#import fasta
+import itertools
 
-  
 
-class FASTAReader(object):  #class can store function, like function organizer
-  def __init__(self, file):
-    self.file = file
-    self.last_id = None
+
+aa = sys.argv[1] #to "x"
+nt = sys.argv[2] #to "y"
+
+#aa = open(sys.argv[1]) #to "x"
+#nt = open(sys.argv[2]) #to "y"
+
+#aa = fasta.FASTAReader(open(sys.argv[1])) #to "x"
+#nt = fasta.FASTAReader(open(sys.argv[2])) #to "y"
+proteins = []
+nucleotides = []
+
+
+for x in aa:
+    proteins.append(x)
     
-  def next(self):
-    if self.last_id is None:  
-      line = self.file.readline()
-      # read only one line
-      # verify is header line
-
-      assert line.startswith(">")
-      #extract ID ---- whole line
-      #identifier = line[1:].rstrip("\r\n")
-  
-      identifier = line[1:].split()[0] #try here
-    else:
-      identifier = self.last_id
-      
-    sequences = []
-
-    while 1: #1 means always True!!!!
-      line = sys.stdin.readline().rstrip("\r\n")
-      if line.startswith(">"): #or line == "": #MODIFY here!!!!
-        self.last_id = line[1:].split()[0]
-        break
-      elif line == "":
-        return None,None
-      else:
-        sequences.append(line)
+for y in nt:
+    nucleotides.append(y)
     
-    return identifier, "".join(sequences)
+for i in itertools.izip(proteins, nucleotides):
+    rev_trl = []
+    n = 0
+    aa_seq = i [0][1]
+    nt_seq = i [1][1]
+    for aa_x in aa_seq:
+        if aa_x == "-":
+            rev_trl.append("---")
+        else:
+            nt_y = nt_seq[ n: n+3 ]
+            n += 3
+            rev_trl.append(nt_y)
+    print ">" + str(i [0][0])
+    print "".join(rev_trl)
 
-
-
-#print identifier, "".join(sequences)
-
-
-
-
-# what i wannt:
-reader = FASTAReader(sys.stdin)
-
-while 1:
-  identifier, sequence = reader.next()
-  if identifier is None:
-    break
-  print identifier, sequence
-  #print sequence
-
-    
-    
